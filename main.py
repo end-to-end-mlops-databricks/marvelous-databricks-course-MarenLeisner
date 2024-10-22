45fbf0cdcf2df3c4f998e4594e36c8bd3686245c
le <maren.leisner@unicef.de>
2024-10-22 17:08:26 +0200

    Well this is the best i can do. I found it very hard. I had never worked with python packages before.

---
+++ main.py
@@ -0,0 +1,28 @@
+import yaml
+import logging
+from databricks.connect import DatabricksSession
+from taxinyc.data_processor import DataProcessor
+
+logging.basicConfig(
+    level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+logger = logging.getLogger(__name__)
+
+spark = DatabricksSession.builder.profile("adb-6130442328907134").getOrCreate()
+
+with open('project_config.yml', 'r') as file:
+    config = yaml.safe_load(file)
+
+print("Configuration loaded:")
+print(yaml.dump(config, default_flow_style=False))
+
+spark = DatabricksSession.builder.profile("adb-6130442328907134").getOrCreate()
+path = "samples.nyctaxi.trips"
+
+# Initialize DataProcessor
+data_processor = DataProcessor(spark, path, config)
+logger.info("DataProcessor initialized.")
+
+# Preprocess the data
+data_processor.preprocess_data()
+logger.info("Data preprocessed.")
